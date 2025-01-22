ca984331d9167ed5685ac5a64719d16670ab26c3
Reinazhard <reinazhard@gmail.com>
2025-01-22 22:44:01 +0700

    perpustakaan: Init
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bookRoutes = require('./routes/books');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5231;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api/books', bookRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
